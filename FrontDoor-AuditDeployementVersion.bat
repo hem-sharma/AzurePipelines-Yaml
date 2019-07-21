4d5307424dd4adf3d085f05cba691b1bb8c995ee
Hemant Sharma <kaushik.hemant@live.com>
2019-07-21 18:46:44 +0530

    Update FrontDoor-AuditDeployementVersion.bat

--- FrontDoor-AuditDeployementVersion.bat
+++ FrontDoor-AuditDeployementVersion.bat
@@ -8,9 +8,8 @@ ls
 
 cd %2
 git checkout %1
-git config --global user.email "kaushik.hemant@live.com"
-git config --global user.name "Hemant Sharma"
-git tag -a test 6bc1dd0 -m 'added'
+
+git tag -a test1 6bc1dd0 -m 'added'
 git push --follow-tags
 
 ECHO completed against !tag!.
