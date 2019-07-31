39debaf2b6e52ddcbd9354f2cf0364ea8c716095
Hemant Sharma <kaushik.hemant@live.com>
2019-07-31 13:48:17 +0530

    Update FrontDoor-AuditDeployementVersion.bat

--- FrontDoor-AuditDeployementVersion.bat
+++ FrontDoor-AuditDeployementVersion.bat
@@ -3,7 +3,7 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 REM Arguments- %1: Root Directory, %2: Region, %3: Timestamp, %4: Environment, %5: Current head, %6: Global region
 ECHO ==============================================================================
 ECHO Initiating...
-EXIT /b 1
+
 
 ECHO completed against !tag!.
 ECHO ==============================================================================
